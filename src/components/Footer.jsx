b1a1423a47cf1de8407c1c9807ae37ba12f27ce3
Md. Shoriful Islam <shorifulislam776@gmail.com>
2025-04-24 23:30:37 +0600

    secound tim656e

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-white dark:bg-[#28282B] p-6 text-center dark:text-white'>
-      <p>&copy; 2024 GymName. All rights reserved.</p>
+      <p>&copy; 2025 GymName. All rights reserved.</p>
     </footer>
   )
 }
